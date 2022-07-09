7c3a22822f60f22450cdbb6b4a3fddc1129f510f
joannator-martynow <joannator.gda@gmail.com>
2022-07-09 14:04:40 +0200

    adding new file "nowy-dokument.txt"

---
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -0,0 +1,14 @@
+package pl.cyber.trainees;
+
+import java.util.Scanner;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+       String firstName = "Joanna";
+       String lastName = "Tor-Martynow";
+
+       System.out.println(firstName + lastName);
+
+    }
+
+}
