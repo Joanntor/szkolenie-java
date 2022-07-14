10f9941247528dcb9d15393f9f131b932bea8236
joannator-martynow <joannator.gda@gmail.com>
2022-07-14 23:32:12 +0200

    adding new file SimpleApp.java.java

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,15 +1,15 @@
 package pl.cyber.trainees;
+// określa w ramach jakiej struktury katalogów opiera się nasza klasa javova
 
 import java.util.Scanner;
 
-public class SimpleApp {
+public class SimpleApp { //nazwa klasy. Modyfikatorem klasy jest public - co znaczy, że klasa jest dostępna dla całego projektu
+
     public static void main(String[] args) {
-       int a = 2;
-       int b = 3;
-
-       System.out.println(a+b);
-
-
+        System.out.println("My first Application");
+        //system.out oznacza, że będziemy coś lokowali na zewnątrz
+        // println, że będzie to do terminala
+        // ln na końcu oznacza, że będzie to z enterem na końcu. Samo print - bez ln to bez entera na końcu
     }
 
 }
