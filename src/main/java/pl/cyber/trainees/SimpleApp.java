4130ef251a065b59edb7ff2a0f22455e67f765b2
joannator-martynow <joannator.gda@gmail.com>
2022-07-15 09:04:37 +0200

    operacje na tekstach

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -29,30 +29,44 @@ public class SimpleApp { //nazwa klasy. Modyfikatorem klasy jest public - co zna
         // println, że będzie to do terminala
         // ln na końcu oznacza, że będzie to z enterem na końcu. Samo print - bez ln to bez entera na końcu
 
-    int intValue = 4000000;
-    int intValueB = intValue + 1000;
+        int intValue = 4000000;
+        int intValueB = intValue + 1000;
 
-    System.out.println(intValue);
-    System.out.println(intValue + 100);
-    System.out.println(intValueB);
+        System.out.println(intValue);
+        System.out.println(intValue + 100);
+        System.out.println(intValueB);
 
-    double doubleValue = 2.0;
-    System.out.println(doubleValue);
+        double doubleValue = 2.0;
+        System.out.println(doubleValue);
 
-    char znak = 'a';
-    char[] tabZnakow = {'\u0043', 'a'};
+        char znak = 'a';
+        char[] tabZnakow = {'\u0043', 'a'};
+
+        System.out.println("add: " + add(1, 3));
+        System.out.println("concat: " + concat("Jan ", "Nowak"));
+
+        System.out.println("\nLiterały i \tznaki specjalne");
+
+    }
     // to mi się nie wyświetla
 
-    Stan stan = Stan
+    public static int add(int l1, int l2) {
+      var result = l1 + l2;
+       return result;
+
+        }
+        public static String concat(String l1, String l2) {
+         var result = l1 + l2;
+         return result;
+       }
 
 
-
-
-} }
+}
 
 // ctr + lewy przycisk myszy na String - wywołuje komentarz dokumentujący
 
 //zmienne
+
 
 
 
