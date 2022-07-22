0861ca34cecc344d35872c863624635346ad6020
joannator-martynow <joannator.gda@gmail.com>
2022-07-22 20:06:20 +0200

    struktury klas i metod w java

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,4 +1,16 @@
-package pl.cyber.trainees;
+// 1. konstrukcja klas i metod
+package pl.cyber.trainees; //okreslenie pakietu w ramach r]projektu, gdzie znajjduje się nasza klasa
+
+//pole package ustawia się automatycznie jeśli nasz plik zawiera się w odpwiedniej strukturze katalogów
+// tj. src/main/java.......
+//w naszym przykładzie src/main/java/pl.cyber.trainees
+
+//tu jest miejsce na importy
+// import java.io.File; //region miejsce na importy import java.io.File;  //importy zazwyczaj dodają się same ewemtualnie można je dodać poprzez Ctrl + Enter 
+// poprzez importy powinniśmy rozumieć użycie dodatkowych bibliotek w naszej klasie/
+import java.math.*;
+// z * mówimy że chcemy zaimportować wszystkie biblioteki wramach pakietu (package) w tym przypadku java.math
+// /endregion
 
 public class SimpleApp {
 
